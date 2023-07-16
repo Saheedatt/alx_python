f416694b3ff38f558ebf5ea954145ed022fa817d
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-16 19:20:55 +0100

    Task 6

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_combinations(start_digit, end_digit):
+    for digit1 in range(start_digit, end_digit + 1):
+        for digit2 in range(digit1 + 1, end_digit + 1):
+            print("{}{}".format(digit1, digit2), end=", ")
+
+
+def main():
+    print_combinations(0, 9)
+
+
+if __name__ == "__main__":
+    main()
