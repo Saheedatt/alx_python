cf258c2c881e1cd594b8403612ccde95f9bdc851
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-16 18:12:07 +0100

    Task 3 -updated

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
@@ -9,6 +10,8 @@ if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
     print("and is 0")
-else:
+elif last_digit < 6 and last_digit != 0:
     print("and is less than 6 and not 0")
-    
+else:
+    print("and is not defined")
+  
