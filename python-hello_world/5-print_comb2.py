78de500e8d8616730f87ecaee7a77d19dfd8c432
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-17 18:08:56 +0100

    Deleted files

--- python-hello_world/5-print_comb2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-for number in range(100):
-    if number < 10:
-        print("0{:02}".format(number), end=", ")
-    else:
-        print("{:02}".format(number), end=", ")
-
-print()
