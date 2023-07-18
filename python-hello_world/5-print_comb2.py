21e2827a8a6f32bc7de6eab0ca6149a3efa80637
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-18 20:11:45 +0100

    Task 5

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for number in range(100):
-        print("{:02}".format(number), end="")
-        if number != 99:
-            print(", ", end="")
+        print("{:02}".format(number), end=",")
+        if number == 99:
+            print("")
