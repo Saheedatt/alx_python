176e91dad0fc1e4fa0ec70a81832c03a0129e0d0
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-20 21:06:46 +0100

    Final attempt

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -3,13 +3,13 @@
 def pow(a, b):
     if b == 0:
         return 1
-    elif b < 0:
-        a = 1 / a
-        b = -b
     result = 1
+    is_negative_power = b < 0
+    b = abs(b)
+
     while b > 0:
         if b % 2 == 1:
             result *= a
         a *= a
         b //= 2
-    return result
+    return 1 / result if is_negative_power else result
