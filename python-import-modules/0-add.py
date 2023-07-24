59591f3290073ebfe80a6114103029eb88831e17
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-24 17:50:07 +0100

    Task 0

--- python-import-modules/0-add.py
+++ python-import-modules/0-add.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 a = 1
 b = 2
 
 # Importing the add function from add_0.py
-from add_0 import add
+from add_0 import add as FAKE_add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, FAKE_add(a, b)))
