f5a505bd1d4c0f55e74fecf34879998ad5ae4041
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-25 12:58:27 +0100

    Task 5

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
-def raise_exception():
-    raise TypeError("This is a type exception!")
+def raise_exception_msg(message=""):
+    if not isinstance(message, str):
+        raise TypeError("Message should be a string")
+    if message.lower() == "python is cool":
+        raise NameError("NameError: Python is cool")
+    elif message.lower() == "c is fun":
+        raise NameError("NameError: C is fun")
+    elif message.lower() == "":
+        raise NameError("NameError: No message provided")
+    else:
+        raise NameError("NameError: " + message)
 
 try:
-    raise_exception()
-except TypeError as te:
-    print("Exception has been raised")
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
+
+try:
+    raise_exception_msg("Python is cool")
+except NameError as ne:
+    print(ne)
+
+try:
+    raise_exception_msg("")
+except NameError as ne:
+    print(ne)
