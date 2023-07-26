647f55301271e486b7415512d0f658dabfff734b
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-26 15:34:52 +0100

    5

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,18 +1,3 @@
 #!/usr/bin/env python3
 def raise_exception_msg(message=""):
     raise NameError(message)
-
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
-
-try:
-    raise_exception_msg("Python is cool")
-except NameError as ne:
-    print(ne)
-
-try:
-    raise_exception_msg("")
-except NameError as ne:
-    print(ne)
