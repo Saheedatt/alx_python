c0c3f1b1ffa8da7b0b519c2fbe98c603c9d2ec12
Saheedatt <afolabisaheedat1@gmail.com>
2023-07-26 15:58:32 +0100

    Data Structure

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        formatted_row = ""
-
         for element in row:
-            formatted_row += "{:d} ".format(element)
+            print("{:d}".format(element), end=" ")
 
-        print(formatted_row)
+        print()
