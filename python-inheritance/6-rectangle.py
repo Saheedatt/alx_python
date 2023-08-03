1ab882823f85c93c774c042f4af80b20ff5b4574
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-03 13:34:30 +0100

    Task 6

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -41,27 +41,6 @@ class BaseGeometry:
         """
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """
-        Validates the value as an integer and raises exceptions if invalid.
-
-        Parameters:
-            name (str): The name of the value to be validated.
-            value: The value to be validated.
-
-        Raises:
-            TypeError, if the value is not an integer.
-            ValueError, if the value is <= 0.
-
-        Returns:
-            None
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
 class Rectangle(BaseGeometry):
     """
     This class represents a Rectangle.
@@ -90,3 +69,24 @@ class Rectangle(BaseGeometry):
         self.__height = height
         self.integer_validator(self.__width, "width")
         self.integer_validator(self.__height, "height")
+
+    def integer_validator(self, value, attribute):
+        """
+        Validates the value as an integer and raises exceptions if invalid.
+
+        Parameters:
+            name (str): The name of the value to be validated.
+            value: The value to be validated.
+
+        Raises:
+            TypeError, if the value is not an integer.
+            ValueError, if the value is <= 0.
+
+        Returns:
+            None
+        """
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError(f"{attribute} must be a positive integer")
+        
+    def area(self):
+        return self.__width * self.__height
