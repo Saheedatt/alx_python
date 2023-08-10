a718f6acaa389217fc984848ebb8b630ee4f2a72
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-10 20:25:55 +0100

    Task 4

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -7,37 +7,47 @@ This script takes a letter as input and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
 It processes the response based on JSON formatting and content.
 
-Usage: python search_user.py <letter>
+
 """
 
 import sys
 import requests
 
-def search_user(letter):
+def main():
     """
     Sends a POST request to search for a user based on the provided letter.
 
+    Usage: python search_user.py <letter>
+
     Args:
         letter (str): The letter to use as a search parameter.
+
+    Returns:
+        None
     """
-    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) != 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    #prepare the data to be sent in the POST request
     data = {"q": letter}
 
     try:
-        response = requests.post(url, data=data)
-        json_data = response.json()
-
-        if isinstance(json_data, dict) and json_data:
-            user_id = json_data.get("id", "N/A")
-            user_name = json_data.get("name", "N/A")
-            print(f"[{user_id}] {user_name}")
-        elif not json_data:
-            print("No result")
-        else:
+        #send a POST request to a specified URL with the letter data
+        response =requests.post("http://0.0.0.0:5000/search_user", data = data)
+        #Parse the JSON response
+        try:
+            json_response = response.json()
+            if isinstance(json_response, dict) and 'id' in json_response and 'name' in json_response:
+                print("[{}] {}".format(json_response['id'], json_response['name']))
+            else:
+                print("No result")
+        except ValueError:
             print("Not a valid JSON")
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+    except requests.exceptions.RequestException as e:
+        print("Request Error:", e)
+    
 
 if __name__ == "__main__":
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    search_user(letter)
+    main()
