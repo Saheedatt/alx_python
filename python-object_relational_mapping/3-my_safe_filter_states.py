a2c8f9f90feda62c4883e31169f6cf9571d99ace
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-21 18:30:09 +0100

    Attempt 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,18 @@
 """
-Script to list all states with a name starting with N (case-sensitive) from
-the hbtn_0e_0_usa database.
+Script to list states from the hbtn_0e_0_usa database based on search criteria.
 
 Usage:
-    python script.py <username> <password> <database>
+    python script.py <username> <password> <database> <search_name>
 
 This script connects to a MySQL server running on localhost at port 3306.
-It retrieves a sorted list of states from the specified database
+It retrieves a sorted list of states from the specified database based on the search criteria
 and displays them.
 
 Arguments:
     <username>: MySQL username.
     <password>: MySQL password.
     <database>: Database name.
+    <search_name>: State name to search for.
 """
 
 
@@ -20,16 +20,15 @@ import MySQLdb
 import sys
 
 
-def list_states_with_name(username, password, search_name):
+def list_states_with_name(username, password, database, search_name):
     """
-    Fetches and lists all states from the hbtn_0e_0_usa database with
-    the given name.
-
+    Fetches and lists states from the hbtn_0e_0_usa database based on search criteria.
+    
     Args:
         <username>: MySQL username.
         <password>: MySQL password.
         <database>: Database name.
-        <state_name>: Name of the state to search for.
+        <search_name>: State name to search for.
     Returns:
         None
     """
@@ -45,17 +44,16 @@ def list_states_with_name(username, password, search_name):
         ) as connection:
             # Create a cursor that aids interaction with the database
             cursor = connection.cursor()
-            # Execute the SQL query to retrieve states using parameterized
-            # query
+            # Execute the SQL query to retrieve states using parameterized query
             query = (
                 "SELECT * FROM states "
                 "WHERE name = %s "
                 "ORDER BY states.id ASC"
             )
-            cursor.execute(query, (state_name,))
+            cursor.execute(query, (search_name,))
             # Fetch all the rows from the result
             rows = cursor.fetchall()
-  
+
             # Print results
             for row in rows:
                 print(row)
@@ -64,11 +62,10 @@ def list_states_with_name(username, password, search_name):
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database> "
-              "<state_name>")
+        print("Usage: python script.py <username> <password> <database> <search_name>")
     else:
         username = sys.argv[1]
         password = sys.argv[2]
         database = sys.argv[3]
-        state_name = sys.argv[4]
-        list_states_with_name(username, password, database, state_name)
+        search_name = sys.argv[4]
+        list_states_with_name(username, password, database, search_name)
