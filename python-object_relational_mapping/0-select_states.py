baedaf82412711704b533611427a851c1202b95b
Saheedatt <afolabisaheedat1@gmail.com>
2023-08-21 14:18:06 +0100

    Get all states fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,8 @@ Usage:
     python script.py <username> <password> <database>
 
 This script connects to a MySQL server running on localhost at port 3306.
-It retrieves a sorted list of states from the specified database and displays them.
+It retrieves a sorted list of states from the specified database
+and displays them.
 
 Arguments:
     <username>: MySQL username.
@@ -15,10 +16,11 @@ Arguments:
 
 import MySQLdb
 
+
 def list_states(username, password, database):
     """
     Fetches and lists all states from the hbtn_0e_0_usa database
-    
+
     Args:
         <username>: MySQL username.
         <password>: MySQL password.
@@ -27,25 +29,27 @@ def list_states(username, password, database):
         None
     """
     try:
-        #connect to the server using context manager
+        # connect to the server using context manager
         with MySQLdb.connect(
             host='localhost', user=username, passwd=password, db=database, port=3306
         )as connection:
-            #create a cursor that aids interaction with database
+            # create a cursor that aids interaction with database
             cursor = connection.cursor()
-            #execute th sql query to retrieve states
+            # execute th sql query to retrieve states
             query = "SELECT * FROM states ORDER BY states.id ASC"
             cursor.execute(query)
-        #fetch all the rows from the result
+        # fetch all the rows from the result
         rows = cursor.fetchall()
 
-        #print results
-        for row in rows:
-            print(row)
+        if len(rows) ==0:
+            print([])
+        else:
+            for row in rows:
+                print(row)
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
     finally:
-    # Close the cursor object
+        # Close the cursor object
         cursor.close()
     # Close the connection to the database
         connection.close()
