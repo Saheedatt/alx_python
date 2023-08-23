8fb92473c563a297e3211c5fb3e11be3f7843572
saheedatt <afolabisaheedat1@gmail.com>
2023-08-23 13:40:31 +0100

    Validation done!

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,6 +3,14 @@ Script to list all State objects from the hbtn_0e_6_usa database.
 
 This script connects to a MySQL server running on localhost at port 3306
 and retrieves a sorted list of State objects from the specified database.
+
+Usage:
+    state_listing.py <mysql username> <mysql password> <database name>
+
+Arguments:
+    <mysql username>    MySQL username for database connection.
+    <mysql password>    MySQL password for the specified username.
+    <database name>     Name of the database to connect to.
 """
 
 
@@ -13,25 +21,27 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """
-    Fetches and lists all State objects from the hbtn_0e_6_usa database.
+    List all State objects from the specified MySQL database.
 
     Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
+        username (str): MySQL username for database connection.
+        password (str): MySQL password for the specified username.
+        db_name (str): Name of the database to connect to.
+
     Returns:
-        None
+        list: A list of State objects sorted by their IDs in ascending order.
     """
 
-
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+            .format(sys.argv[0]))
         sys.exit(1)
 
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Create a connection to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, db_name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name))
 
     # Bind the engine to the Base class to enable declarative classes
     Base.metadata.create_all(engine)
@@ -40,7 +50,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the database and list all State objects in ascending order by states.id
+    # Query the database and list all State objects in asc order by states.id
     states = session.query(State).order_by(State.id).all()
 
     # Display the results
