4ee21ec890e12ae0adc79dac0bddcc8f1164d5f4
saheedatt <afolabisaheedat1@gmail.com>
2023-08-23 13:42:17 +0100

    Indentation error

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: {} <mysql username> <mysql password> <database name>"
-            .format(sys.argv[0]))
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
