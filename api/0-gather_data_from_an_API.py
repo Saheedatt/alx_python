3bb47cbab05b2b6787bbd243c1329f1aaabe4b65
saheedatt <afolabisaheedat1@gmail.com>
2023-10-02 17:29:38 +0100

    Sighs

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,11 +1,10 @@
 import json
-import sys
 import urllib.request
 
 def get_employee_data(employee_id):
     # Endpoint URLs for employee details and todo list
-    employee_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
     # Fetch employee details
     with urllib.request.urlopen(employee_url) as response:
@@ -17,16 +16,13 @@ def get_employee_data(employee_id):
         todo_data = json.loads(response.read().decode())
 
     # Display employee TODO list progress
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, sum(task['completed'] for task in todo_data), len(todo_data)))
-
-    # Display each task
+    print(f"Employee {employee_name} is done with tasks({sum(1 for task in todo_data if task['completed'])}/{len(todo_data)}):")
     for i, task in enumerate(todo_data, start=1):
-        print("Task {} Formatting: {}".format(i, 'OK' if task['completed'] else 'Incorrect'))
+        print(f"Task {i} Formatting: {'OK' if task['completed'] else 'Incorrect'}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_data(employee_id)
+    try:
+        employee_id = int(input("Enter the employee ID: "))
+        get_employee_data(employee_id)
+    except ValueError:
+        print("Employee ID should be an integer.")
