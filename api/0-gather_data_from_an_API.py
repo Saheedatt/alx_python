3fd3ba4a4f11fbe38bee171d50e79717e7e92439
saheedatt <afolabisaheedat1@gmail.com>
2023-10-02 17:20:46 +0100

    Still failing

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,14 +10,16 @@ def get_employee_data(employee_id):
     # Fetch employee details
     with urllib.request.urlopen(employee_url) as response:
         employee_data = json.loads(response.read().decode())
-        employee_name = employee_data['name']
+        employee_name = employee_data.get('name', 'Unknown Employee')
 
     # Fetch todo list
     with urllib.request.urlopen(todo_url) as response:
         todo_data = json.loads(response.read().decode())
 
     # Display employee TODO list progress
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, sum(1 for _ in todo_data if True), len(todo_data)))
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, sum(task['completed'] for task in todo_data), len(todo_data)))
+
+    # Display each task
     for i, task in enumerate(todo_data, start=1):
         print("Task {} Formatting: {}".format(i, 'OK' if task['completed'] else 'Incorrect'))
 
