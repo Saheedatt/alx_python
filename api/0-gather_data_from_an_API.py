549d86f35f51afa1b393afaaf1c2e369aa84c857
saheedatt <afolabisaheedat1@gmail.com>
2023-10-02 16:57:31 +0100

    Regathering of API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,29 +1,24 @@
-import sys
 import requests
+import sys
 
 def get_employee_data(employee_id):
-    # Endpoint URLs for emplyee details and todo list
+    # Endpoint URLs for employee details and todo list
     employee_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
     todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
 
-    #Fetch employee details
+    # Fetch employee details
     employee_response = requests.get(employee_url)
     employee_data = employee_response.json()
     employee_name = employee_data['name']
 
-    #to fetch todo list
+    # Fetch todo list
     todo_response = requests.get(todo_url)
     todo_data = todo_response.json()
 
-    #count completed tasks
-    completed_tasks = [task for task in todo_data if task['completed']]
-    num_completed_tasks = len(completed_tasks)
-    total_tasks = len(todo_data)
-
     # Display employee TODO list progress
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed_tasks, total_tasks))
-    for task in completed_tasks:
-        print("    {}".format(task['title']))
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, sum(1 for task in todo_data if task['completed']), len(todo_data)))
+    for i, task in enumerate(todo_data, start=1):
+        print("Task {} Formatting: {}".format(i, 'OK' if task['completed'] else 'Incorrect'))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
