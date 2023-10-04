aaa4eb2f07861590df5f517d4231fc05019085cb
saheedatt <afolabisaheedat1@gmail.com>
2023-10-04 20:11:02 +0100

    Commit 102

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,36 +2,29 @@ import requests
 import sys
 
 if len(sys.argv) != 2:
-    print("First line formatting: Incorrect")
     print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
     sys.exit(1)
 
 employee_id = sys.argv[1]
-url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+url_employee = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+url_todos = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-response = requests.get(url)
-
-if response.status_code != 200:
-    print("First line formatting: Incorrect")
-    print(f"No employee found with ID {employee_id}")
-    sys.exit(1)
-
-employee_data = response.json()
+# Get employee details
+response_employee = requests.get(url_employee)
+employee_data = response_employee.json()
 employee_name = employee_data.get("name")
 
 if not employee_name:
-    print("First line formatting: Incorrect")
     print(f"No employee found with ID {employee_id}")
     sys.exit(1)
 
-url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-response = requests.get(url)
-todos = response.json()
+# Get employee's TODO list
+response_todos = requests.get(url_todos)
+todos = response_todos.json()
 
 total_tasks = len(todos)
 done_tasks = sum(1 for todo in todos if todo.get("completed"))
 
-print(f"First line formatting: OK")
 print(f"Employee {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
 for todo in todos:
     if todo.get("completed"):
