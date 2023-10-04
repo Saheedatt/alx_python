e61c86ed0003d57a8bf7bc2342a45f52407079f8
saheedatt <afolabisaheedat1@gmail.com>
2023-10-04 21:23:58 +0100

    I lied

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,12 +1,11 @@
 import requests
 import sys
 
-if len(sys.argv)!=2:
-    print("Usage: python3 get_employee_todo_list_progress.py <employee_id>")
+if len(sys.argv) !=2:
+    print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
     sys.exit(1)
 
 employee_id = sys.argv[1]
-
 employee_name_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 response = requests.get(employee_name_url)
 employee_name = response.json().get("name")
