470cfc3b56ad9998a302709794a7aaf464bc2ea7
saheedatt <afolabisaheedat1@gmail.com>
2023-10-04 21:17:38 +0100

    Finale

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,6 @@ if len(sys.argv) != 2:
 
 employee_id = sys.argv[1]
 
-# Get employee name
 employee_name_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
 response = requests.get(employee_name_url)
 employee_name = response.json().get("name")
@@ -16,17 +15,14 @@ if not employee_name:
     print(f"No employee found with ID {employee_id}")
     sys.exit(1)
 
-# Get employee TODO list
 employee_todo_list_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 response = requests.get(employee_todo_list_url)
 todos = response.json()
 
-# Calculate TODO list progress
 total_tasks = len(todos)
 done_tasks = sum(1 for todo in todos if todo.get("completed"))
 
-# Display TODO list progress
 print(f"Employee {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
 for todo in todos:
     if todo.get("completed"):
-        print(f"\t{todo.get('title')}")
+        print(f"\t {todo.get('title')}")
