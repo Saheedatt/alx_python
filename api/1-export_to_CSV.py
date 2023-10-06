ae8aa8ddda48d8e9b4fa2d89243fffd98d805c2c
saheedatt <afolabisaheedat1@gmail.com>
2023-10-06 10:38:04 +0100

    Export to csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,36 +1,25 @@
+#!/usr/bin/python3
 import csv
+import json
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-    sys.exit(1)
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
+    data = api_request.text
+    pjson = json.loads(data)
+    data1 = api_request1.text
+    pjson1 = json.loads(data1)
 
-employee_id = sys.argv[1]
-url_user = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-response_user = requests.get(url_user)
-user_data = response_user.json()
-employee_name = user_data.get("name")
-
-if not employee_name:
-    print(f"No employee found with ID {employee_id}")
-    sys.exit(1)
-
-url_todos = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-response_todos = requests.get(url_todos)
-todos = response_todos.json()
-
-total_tasks = len(todos)
-done_tasks = sum(1 for todo in todos if todo.get("completed"))
-
-csv_file_name = f"{employee_id}.csv"
-
-# Write data to CSV
-with open(csv_file_name, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-    for todo in todos:
-        task_completed_status = "Completed" if todo.get("completed") else "Not Completed"
-        writer.writerow([employee_id, employee_name, task_completed_status, todo.get("title")])
-
-print(f"Data exported to {csv_file_name}.")
+    #export data to csv data
+    filename = "{}.csv".format(employee_id)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
+        for item in pjson1:
+            user_id = employee_id
+            username = pjson['username']
+            task_completed_status = item['completed']
+            task_title = item['title']
+            writer.writerow([user_id, username, task_completed_status, task_title])
